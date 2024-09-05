e425292e12f0a17223a47ad58007ea4992ca0085
Awsaf <awsaf49@gmail.com>
2024-09-05 06:20:47 +0600

    add: codes

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
 # SONICS: Synthetic Or Not - Identifying Counterfeit Songs
-
-<div align="center">
-<img src="assets/thumbnail.jpg" width="300">
-</div>
---
+++ sonics/__init__.py
@@ -0,0 +1,4 @@
+from sonics.utils.seed import set_seed
+from sonics.utils.config import dict2cfg
+from sonics.utils.dataset import get_dataloader
+from sonics.utils.scheduler import get_scheduler
